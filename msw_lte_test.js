680f507e559c1b6413e888f04ddec9c755d3a634
dnjstjr93 <dnjstjr93@gmail.com>
2020-09-09 22:56:22 +0900

    - clear

--- msw_lte_test.js
+++ msw_lte_test.js
@@ -240,7 +240,6 @@ function parseDataMission(topic, str_message) {
 
         var topic_arr = topic.split('/');
         var data_topic = '/Mobius/' + config.gcs + '/Mission_Data/' + config.drone + '/' + config.name + '/' + topic_arr[topic_arr.length-1];
-        console.log('\nDataMission Topic: ', config.sortie_name);
         msw_mqtt_client.publish(data_topic + config.sortie_name, str_message);
     }
     catch (e) {
